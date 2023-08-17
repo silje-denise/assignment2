ee32679e87ddc7b7b97558e7bf8e15fb74a7210f
silje denise <siljedenise@gmail.com>
2023-08-17 15:15:07 +0200

    fix: added basic components on login page

--- lost-in-translation/src/pages/Login.jsx
+++ lost-in-translation/src/pages/Login.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Header from "../components/Header";
 
 const Login = () => {
+    /*
+        TODO: 
+            - Connect to API
+            - Add a CreateUser function
+            - Add a GetUser function
+            - Redirect user to /translation page after being logged in
+    */
+
+    const [username, setUsername ] = useState(""); 
+
+    const handleOnclick = () => {
+        alert(username);
+    }
+    const handleChange = (event) => {
+        setUsername(event.target.value);
+    }
+  
+
     return(
         <div>
-            <Header></Header>
-            <main>test</main>
+            <Header/>
+            <main>
+                <div>
+                    <input type="text" onChange={handleChange}/>
+                    <button onClick={handleOnclick}>➡️</button>
+                </div>
+            </main>
         </div>
     );
 }
