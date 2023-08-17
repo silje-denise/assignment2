0ace68928061ee0cfbe5c0b72bb73d388d398eab
“Silje <siljeeslettebakk@gmail.com>
2023-08-17 21:39:02 +0200

    add react router

---
+++ lost-in-translation/src/pages/Translation.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const Login = () => {
+    return(
+        <div>
+            <Header></Header>
+            <main>Translation Page</main>
+        </div>
+    );
+}
+
+export default Login;
