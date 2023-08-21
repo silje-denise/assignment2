1f7d5ab4ec9246fc8ea4e209980d1050ac29502c
silje denise <siljedenise@gmail.com>
2023-08-21 16:36:11 +0200

    feat: added API connection

--- lost-in-translation/src/pages/Login.jsx
+++ lost-in-translation/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import InputWithBtn from "../components/InputWithBtn";
 
 const Login = () => {
-    /*
+  /*
         TODO: 
             - Connect to API
             - Add a CreateUser function
@@ -11,77 +11,102 @@ const Login = () => {
             - Redirect user to /translation page after being logged in
     */
 
-    const [username, setUsername ] = useState(""); 
+  const [username, setUsername] = useState("");
 
-    const handleOnclick = () => {
-        alert(username);
-    }
-    const handleChange = (event) => {
-        setUsername(event.target.value);
-        //Run createUser
-    }
+  const handleOnclick = () => {
+    alert(username);
+    getUser();
+    // if(){
+    // createUser();
+    // }
 
-    //Create user---------------------------------
-    //Async-await & try-catch?
-    const apiURL = 'your-api-url-goes-here';
-    const apiKey = 'your-public-api-key-goes-here';
+    //TODO: redirect user to /translations
+  };
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
 
+  //Create user---------------------------------
+  //Async-await & try-catch?
+  const apiURL = "https://zenith-oil-snake.glitch.me/";
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  const getUser = () => {
+    fetch(`${apiURL}/translations?username=${username}`)
+      .then((response) => {
+         response.json();
+       })
+      .then((results) => {
+        results.map((r) => {
+          console.log(r);
+          return r;
+        });
+
+        return results;
+        // results will be an array of users that match the username
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const createUser = () => {
     fetch(`${apiURL}/translations`, {
-        method: 'POST',
-        headers: {
-          'X-API-Key': apiKey,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ 
-            username: {username}, 
-            translations: [] 
-        })
+      method: "POST",
+      headers: {
+        "X-API-Key": apiKey,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: { username },
+        translations: [],
+      }),
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Could not create new user');
-      }
-      return response.json();
-    })
-    .then(newUser => {
-      // newUser is the new user with an id
-      console.log(newUser);
-    })
-    .catch(error => {
-    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Could not create new user");
+        }
+        return response.json();
+      })
+      .then((newUser) => {
+        // newUser is the new user with an id
+        console.log(newUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
+  //Styling
+  const main = {
+    display: "flex",
+    justifyContent: "center",
+    padding: "20px",
+    backgroundColor: "lightpink",
+    fontSize: "30px",
+  };
+  const inputSection = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    padding: "20px",
+    backgroundColor: "white",
+    margin: "0 50px",
+    fontSize: "23x",
+    borderRadius: "15px",
+  };
 
-    //Styling
-    const main = {
-        display: "flex", 
-        justifyContent: "center", 
-        padding: "20px", 
-        backgroundColor: "lightpink", 
-        fontSize: "30px",
-    }
-    const inputSection = {
-        display: "flex", 
-        justifyContent: "center", 
-        flexDirection: "column",
-        padding: "20px", 
-        backgroundColor: "white", 
-        margin: "0 50px",
-        fontSize: "23x",
-        borderRadius: "15px"
-    }
-  
-
-    return(
-        <div>
-            <Header/>
-            <main style={main}>
-                <div style={inputSection}>
-                    <label>Let's get started! Type in a username:</label>
-                    <InputWithBtn onchange={handleChange} onclick={handleOnclick}/>
-                </div>
-            </main>
+  return (
+    <div>
+      <Header />
+      <main style={main}>
+        <div style={inputSection}>
+          <label>Let's get started! Type in a username:</label>
+          <InputWithBtn onchange={handleChange} onclick={handleOnclick} />
         </div>
-    );
-}
+      </main>
+    </div>
+  );
+};
 
 export default Login;
